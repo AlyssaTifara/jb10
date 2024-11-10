7679b5efde691ae7dc8092c3910e5ff3bafe85f0
AlyssaTifara <tissaalyssa@gmail.com>
2024-11-10 16:18:44 +0700

    pk 1

---
+++ Demo.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom M.Kom", "199402");
+        Dosen dosen2 = new Dosen("19700105", "Alyssa, S.T, M.T", "197001");
+        TenagaKependidikan tkp1 = new TenagaKependidikan("19750301", "Aida, A.Md", "Tenaga Kependidikan");
+        TenagaKependidikan tkp2 = new TenagaKependidikan("19650304", "Rika, S.T", "Tenaga Kependidikan");
+        
+        ArrayList<Pegawai> daftarpegawai = new ArrayList<Pegawai>();
+
+        daftarpegawai.add(dosen1);
+        daftarpegawai.add(dosen2);
+        daftarpegawai.add(tkp1);
+        daftarpegawai.add(tkp2);
+
+        System.out.println("Jumlah Pegawai: " + daftarpegawai.size());
+    }
+}
